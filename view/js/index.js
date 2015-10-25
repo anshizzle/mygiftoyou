5232b86e7660ceba0fa9892a3e9d88c28a047267
mlindsay <mlindsay@switchfly.com>
2015-10-25 20:23:31 +0000

    pushing js stuff

--- view/js/index.js
+++ view/js/index.js
@@ -28,6 +28,13 @@ $(document).ready(function(){
     // }
 
 var gifHistory = []
+var subredditScores = {
+                      'gifs': 0, 'physicsgifs': 0, 'naturegifs': 0, 
+                      'instant_regret': 0, 'rectiongifs': 0, 'aviationgifs': 0,
+                      'dashcamgifs': 0, 'educationalgifs': 0, 'holdmybeer': 0,
+                      'nevertellmetheodds': 0, 'nonononoaww': 0, 'oceangifs': 0,
+                      'spacegifs': 0, 'startledcats': 0, 'wellthatsucks': 0, 'mechanical_gifs': 0
+                      }
 
 var jsonFile;
 
@@ -39,7 +46,7 @@ function getURL() {
     //   returnFile = "hi";
     // });
 
-    jsonFile = $.getJSON("js/test.json");
+    jsonFile = $.getJSON("js/gif_urls.json");
 }
 
 getURL();
@@ -48,21 +55,44 @@ jsonFile.done( function() {
   console.log(jsonFile);  
 });
 
-
-    
-
-    document.getElementById("next").src = "http://thefw.com/files/2013/05/0hgyoKm.gif";
+{debugger}
+var index = 0;
+var subredditCounter = 0;
+var url = jsonFile['gifs'][index];
+var subreddit = 'gifs';
+var keys = Object.keys(jsonFile);
+document.getElementById("next").src = url;
 
     //swiping functionality:
     $(".buddy").on("swiperight",function(){
       $(this).addClass('rotate-left').delay(700).fadeOut(1);
       $('.buddy').find('.status').remove();
 
-      $(this).append('<div class="status like">Like!</div>');      
-      if ( $(this).is(':last-child') ) {
+      $(this).append('<div class="status like">Like!</div>');
+
+      subredditScores[subreddit]+=1 
+      if(index >= jsonFile[subreddit].length){
+        index = 0;
+
+
+
+        if(subreddit_counter < keys.length){
+          subredditCounter+=1;
+        }else{
+          subredditCounter = 0;
+        }
+
+        subreddit = keys(jsonFile)[subreddit_counter];
+
         $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
        } else {
-          $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
+        subredditScores[subreddit] -= 1
+        if(subredditScores[subreddit] <= -3){
+          subredditCounter+=1
+          index = 0
+          subreddit = keys(jsonFile)[subredditCounter]
+        }
+        $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
        }
     });  
 
