6715a110f43f5b14f6d62dc91b447f9bb8d9e1d2
Chris Atanasian <catanasian@gmail.com>
2015-10-24 22:52:20 -0700

    Filter for gifs

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -1,8 +1,10 @@
-#!usr/bin/env
+#!usr/bin/env/python
 
 import praw
 
 class RedditScraper:
+	ACCEPTABLE_FILE_EXTENSIONS = ['gif', 'gifv']
+
 	def __init__(self):
 		self.user_agent = praw.Reddit(user_agent='my_gift_to_you')
 
@@ -12,11 +14,40 @@ class RedditScraper:
 	def get_submissions_top(self, subreddit, count):
 		return self.user_agent.get_subreddit(subreddit).get_top(limit=count)
 
+	def is_gif(self, submission):
+		url = submission.url
+
+		if '.' in url:
+			file_extension = url[url.rindex('.')+1:]
+			if file_extension in self.ACCEPTABLE_FILE_EXTENSIONS:
+				return True
+
+		return False
+
+	def is_gfycat(self, submission):
+		url = submission.url
+
+		if 'gfycat.com' in url:
+			return True
+
+		return False
+
 
 def main():
 	r = RedditScraper()
-	for submission in r.get_submissions():
-		print submission.url
+	submissions = r.get_submissions_top('naturegifs', 1)
+
+	for submission in submissions:
+		if r.is_gif(submission):
+			# write to db
+			print submission.url
+			print submission
+		elif r.is_gfycat(submission):
+			# write to db
+			submission.url = submission.url.replace('gfycat.com', 'giant.gfycat.com') + '.gif'
+			print submission.url
+			print submission
+
 
 if __name__ == "__main__":
 	main()
